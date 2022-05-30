34d6b43ef68b51405d6710f629c08b6e5b07b6f7
Ziad Abouelfarah <zain.fake02@gmail.com>
2022-05-30 14:49:20 +0100

    feat: small fix in dashboard

--- src/routes/dashboard.router.ts
+++ src/routes/dashboard.router.ts
@@ -1,40 +1,41 @@
-import SessionClient from "../clients/sessionClient"
-import { Router } from "express"
-import classroomExtractor from "../utils/classroomExtractor"
-import ClassroomClient from "../clients/classroomClient"
+import SessionClient from "../clients/sessionClient";
+import { Router } from "express";
+import classroomExtractor from "../utils/classroomExtractor";
+import ClassroomClient from "../clients/classroomClient";
 
-const router = Router()
-const sessionClient = new SessionClient()
-const classroomClient = new ClassroomClient()
+const router = Router();
+const sessionClient = new SessionClient();
+const classroomClient = new ClassroomClient();
 
 router.get("/", async (req, res) => {
-  const allSession = await sessionClient.getCurrentSessions()
-  const classrooms = classroomExtractor(allSession)
+  const allSession = await sessionClient.getCurrentSessions();
+  const classrooms = classroomExtractor(allSession);
   const classroomsData = await Promise.all(
     classrooms.map(async (el) => {
-      const data = await classroomClient.getClassroom(el.classId)
-      const sessionData = await sessionClient.getAbsence(el.sessionId)
+      const data = await classroomClient.getClassroom(el.classId);
+      const sessionData = await sessionClient.getAbsence(el.sessionId);
+      const attends = data?.students.length! - sessionData.length;
       return {
         id: data?.id,
-        attends: data?.students.length! - sessionData.length,
+        attends: isNaN(attends) ? 0 : attends,
         absence: sessionData.length,
-      }
+      };
     })
-  )
+  );
   const attendances = classroomsData.reduce((a, b) => {
-    return a + b.attends
-  }, 0)
+    return a + (isNaN(b.attends) ? 0 : b.attends);
+  }, 0);
 
   const absences = classroomsData.reduce((a, b) => {
-    return a + b.absence
-  }, 0)
+    return a + b.absence;
+  }, 0);
 
   return res.json({
     attendances,
     absences,
     currentClassrooms: classrooms.length,
     classrooms: classroomsData,
-  })
-})
+  });
+});
 
-export default router
+export default router;
