d9fcf8af4c3f383236ee83ec11e27939a9665cda
Ziad Abouelfarah <zain.fake02@gmail.com>
2022-06-03 18:28:49 +0100

    feat: add unlimited admin once

--- src/cli/app.py
+++ src/cli/app.py
@@ -1,28 +1,36 @@
 from requests import post
 from getpass import getpass
 from schema import validator
+from os import system
 
 endpoint = "https://shen-be.herokuapp.com/"
 
-data = post(f"{endpoint}generate")
-token = data.json()["data"]
+while True:
+    system("cls||clear")
 
-email = input(f"Enter your email:")
-pwd = getpass(f"Enter your password: ")
+    data = post(f"{endpoint}generate")
+    token = data.json()["data"]
 
-is_valid = validator.validate({"email": email, "password": pwd})
-if is_valid:
-    data = post(f"{endpoint}register", json={"email": email,
-                                             "pwd": pwd}, headers={"X-Shen": token})
-    data = data.json()
-    keys = data.keys()
-    if "token" in keys:
-        print("Admin got registered successfully 🥳🥳")
+    email = input(f"Enter your email:")
+    pwd = getpass(f"Enter your password: ")
 
-else:
-    keys = validator.errors.keys()
-    for key in keys:
-        for error in validator.errors[key]:
-            print(f'{key}: {error}')
+    is_valid = validator.validate({"email": email, "password": pwd})
+    if is_valid:
+        data = post(f"{endpoint}register", json={"email": email,
+                                                 "pwd": pwd}, headers={"X-Shen": token})
+        data = data.json()
+        keys = data.keys()
+        if "token" in keys:
+            print("Admin got registered successfully 🥳🥳")
 
-input()
+    else:
+        keys = validator.errors.keys()
+        for key in keys:
+            for error in validator.errors[key]:
+                print(f'{key}: {error}')
+
+    answer = input("Do you want to add an other admin 🤔? [yes/no]: ")
+    if answer.lower().strip() == "yes":
+        pass
+    else:
+        break
